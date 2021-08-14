c9165bec37f9bdf5154685eb1c8f7c8633357d91
Wivon <74855094+Wivon@users.noreply.github.com>
2021-08-14 12:05:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
 const ipc = ipcRenderer
 const { autoUpdater } = require('electron-updater');
+const path = require('path');
 
 let mainWindow;
 
@@ -11,9 +12,9 @@ function createWindow() {
     frame: false,
     resizable: false,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      devTools: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
     },
   });
 
@@ -26,15 +27,10 @@ function createWindow() {
     autoUpdater.checkForUpdatesAndNotify();
   });
 
-  ipcMain.on('openDevTools', () => {
-    console.log('opening developer tools')
-    mainWindow.webContents.openDevTools()
-  })
-
 }
 
 app.on('ready', () => {
-  createWindow();
+  createWindow()
 });
 
 app.on('window-all-closed', function () {
@@ -53,8 +49,11 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
+function restart () {
+  app.relaunch()
+  app.exit()
+}
 // auto updater
-
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
 });
@@ -67,6 +66,67 @@ ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
 
+// main window ipc functions
+ipcMain.on('openDevTools', () => {
+  console.log('opening dev tools')
+  mainWindow.webContents.openDevTools()
+})
+
 ipcMain.on('minimizeApp', () => {
-  console.log('eofzhdizj')
+  mainWindow.minimize()
+  console.log('app minimized')
 });
+
+ipcMain.on('quitApp', () => {
+  app.quit()
+});
+
+ipcMain.on('closeApp', () => {
+  mainWindow.hide()
+});
+
+ipcMain.on('restart', () => {
+  restart()
+});
+
+// tray icon
+const { Menu, Tray } = require('electron')
+
+let tray = null
+
+app.whenReady().then(() => {
+  
+  const trayIcnName = 'logoX512.png';
+  const trayIcnPath = process.env.WEBPACK_DEV_SERVER_URL
+  ? path.join(__dirname, `/app.asar/src/img/${trayIcnName}`)
+  : path.join(__dirname, `/src/img/${trayIcnName}`);
+
+  tray = new Tray(trayIcnPath)
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'restart',
+      click() { restart() }
+    },
+    {
+      label: 'Check for Updates',
+      click() { autoUpdater.checkForUpdatesAndNotify(); }
+    },
+    {
+      label: 'Dev tools',
+      click() { mainWindow.webContents.openDevTools() }
+    },
+    {
+      label: 'Quit',
+      click() { app.quit(); }
+    }
+  ])
+  tray.setToolTip('OG Hub is running in background')
+  tray.setContextMenu(contextMenu)
+  tray.on('click', function () {
+    mainWindow.show()
+  })
+})
+
+// if (app.requestSingleInstanceLock() == false) {
+
+// }
