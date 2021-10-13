9a88c51946880dae298023d1fd3e0e78f6bff4b9
Wivon <brousselilian6@gmail.com>
2021-10-13 21:01:25 +0200

    🆕Updated popup design

--- content/assets/popups.js
+++ content/assets/popups.js
@@ -5,9 +5,10 @@
  * @param {string} buttonsHTML 
  * @param {string} imagePath
  */
-function openPopup(text, blur = true, buttonsHTML='<button onclick="hidePopup()" class="Popupbutton">Cancel</button><hr><button class="Popupbutton">Confirm</button>', imageParams={"path": 'img/close_icon.png', "invert": true}) {
+function openPopup(title, text, blur = true, buttonsHTML='<button onclick="hidePopup()" class="Popupbutton">Cancel</button><hr><button class="Popupbutton">Confirm</button>', imageParams={"path": 'img/close_icon.png', "invert": true}) {
     document.querySelector('.popup').style.display = 'flex';
     document.querySelector('.popup').style.transition = 'all .3 ease-out';
+    document.querySelector('.popup h3').innerHTML = title;
     document.querySelector('.popup p').innerHTML = text;
     document.querySelector('.popup .buttons').innerHTML = buttonsHTML;
     document.querySelector('.popup img').src = imageParams.path;
