50481beed75a788a68433d9dbf6284ada2748387
Wivon <brousselilian6@gmail.com>
2021-11-11 18:56:15 +0100

    ➕added unique query selector generator

--- content/assets/formatFunctions.js
+++ content/assets/formatFunctions.js
@@ -14,3 +14,20 @@ function RemoveWhiteSpace(str) {
         return txt.charAt(0).toUpperCase() + txt.substr(1);
     }).replace(/\s/g, "");
 }
+
+function getSelector(elm) {
+    if (elm.tagName === "BODY") return "BODY";
+    const names = [];
+    while (elm.parentElement && elm.tagName !== "BODY") {
+        if (elm.id) {
+            names.unshift("#" + elm.getAttribute("id")); // getAttribute, because `elm.id` could also return a child element with name "id"
+            break; // Because ID should be unique, no more is needed. Remove the break, if you always want a full path.
+        } else {
+            let c = 1, e = elm;
+            for (; e.previousElementSibling; e = e.previousElementSibling, c++);
+            names.unshift(elm.tagName + ":nth-child(" + c + ")");
+        }
+        elm = elm.parentElement;
+    }
+    return names.join(">");
+}
