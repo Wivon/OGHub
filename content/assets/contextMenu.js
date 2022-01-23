bf74a72004fb2193e7c2cb46202b4e1d3a47d57a
Wivon <brousselilian6@gmail.com>
2022-01-23 18:52:25 +0100

    🛠️editing cards

--- content/assets/contextMenu.js
+++ content/assets/contextMenu.js
@@ -37,6 +37,8 @@ function setCardCtxMenuEvent() {
                     document.querySelector('.containerANA .editor input.newCardNameInput').value = card.querySelector('h3').textContent
                     document.querySelector('.containerANA p').textContent = card.querySelector('h3').textContent
                     document.querySelector('.containerANA img').src = card.querySelector('img').src
+                    document.querySelector('.newCardArgsInput').value = "coming soon"
+                    document.querySelector('.newCardArgsInput').classList.add('disabled')
 
                     setTimeout(() => {
                         document.querySelector('.containerANA h2').textContent = "Editing card: " + card.querySelector('h3').textContent
